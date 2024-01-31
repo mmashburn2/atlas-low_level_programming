ad485d157df21b2547b5813bb0d21d088fac6117
mmashburn2 <matt.mashburn@atlasschool.com>
2024-01-31 20:20:39 +0000

    tasting

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,12 +4,13 @@
   *its time to link some strings!
   */
 int i;
+int k;
 char *_strcat(char *dest, char *src)
 {
-	while(src [i] != '\0');
+	while (src [i] != '\0');
 	{
-		dest[i++] = src[i++];
-		dest[i++] = '\0';
+		dest[i] = src[k];
+		dest[i] = '\0';
 	}
 return (dest);
 }
